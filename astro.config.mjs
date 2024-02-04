4fb22317fbe29e6e8f5a341a935bcda22e426424
Mattia Berto <125810753+BertoMVII@users.noreply.github.com>
2024-02-04 19:30:23 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bertomvii.github.io/Agenda-2030/'
-});
+  site: 'https://bertomvii.github.io/Agenda-2030/',
+  base: '/my-repo',
+})
